af506ab4bda2a5c6fd6cb8215330490c7fce3f05
Matheus Barreira Alves <52544520+matheusbarreiraalves@users.noreply.github.com>
2020-06-24 17:20:54 -0300

    Update valorIMC.java

--- valorIMC.java
+++ valorIMC.java
@@ -22,27 +22,22 @@ public class valorIMC {
 		DecimalFormat formatador = new DecimalFormat ("0.00");
 		System.out.println("Cáculo do IMC");
 		//entrada
-		System.out.print("Digite seu peso: ");
+		System.out.print("Digite seu altura: ");
 		peso = leitor.nextDouble();
-		System.out.print("Digite sua altura: ");
+		System.out.print("Digite sua peso: ");
 		altura = leitor.nextDouble();
 		//processamento
 		valor = peso /(altura * altura);
 		//saída
 		System.out.println("IMC: " + formatador.format(valor));
 		if (valor < 18.5) {
-			System.out.println("Magreza Patólogica");
-		}else if(valor >= 20 && valor < 25) {
-			System.out.println("Peso Normal");
-		}else if (valor <= 25){
+			System.out.println("Abaixo do peso");
+		} else if (valor >= 18.5 && valor <= 24.9) {
+			System.out.println("Peso normal");
+		} else {
 			System.out.println("Sobrepeso");
-			
-			}else {
-				System.out.println("Obesidade");
 		}
-		
-		leitor.close();
-		
+        leitor.close();
 	}
 
 }
